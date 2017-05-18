b616d32c244ce27cb7be5a575721f5f8abe2760d
petsvegit <peter.svensson@stenaline.com>
2017-05-18 10:02:50 +0200

    FridgeWebAPI
    REST service for handling fridge.

---
+++ FridgeUnitTest/FridgeTester.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fridge;
+
+
+namespace FridgeUnitTest
+{
+    [TestClass]
+    public class FridgeTester
+    {
+        private string inventoryItemMeatballs = "Meatballs";
+
+        [TestMethod]
+        public void ToEmptyFridgeAddOneInventoryItem()
+        {
+            FridgeWorker currentFridge = new FridgeWorker();
+           
+            currentFridge.AddIngredientToFridge(inventoryItemMeatballs, 10);
+
+        }
+        
+        [TestMethod]
+        public void ToEmptyFridgeAddTwoIdenticalInventoryItem()
+        {
+            FridgeWorker currentFridge = new FridgeWorker();
+
+            currentFridge.AddIngredientToFridge(inventoryItemMeatballs, 10);
+            currentFridge.AddIngredientToFridge(inventoryItemMeatballs, 10);
+
+            Assert.AreEqual( 1, currentFridge.InventoryList.Count);
+            Assert.AreEqual(20, currentFridge.InventoryList[0].Quantity);
+        }
+
+        [TestMethod]
+        public void DoesInventoryItemExist()
+        {
+            FridgeWorker currentFridge = new FridgeWorker();
+
+            currentFridge.AddIngredientToFridge(inventoryItemMeatballs, 10);
+
+            FridgeInventory result = currentFridge.GetInventoryItem(inventoryItemMeatballs);
+            Assert.AreEqual(10, result.Quantity);
+        }
+
+        [TestMethod]
+        public void FromEmptyFridgeIsItemAvailable()
+        {
+            FridgeWorker currentFridge = new FridgeWorker();
+            Assert.AreEqual(false, currentFridge.IsItemAvailable(inventoryItemMeatballs, 7));
+        }
+
+        [TestMethod]
+        public void FromFullFridgeIsItemAvailable()
+        {
+            FridgeWorker currentFridge = new FridgeWorker();
+
+            currentFridge.AddIngredientToFridge(inventoryItemMeatballs, 10);
+
+            Assert.AreEqual(true, currentFridge.IsItemAvailable(inventoryItemMeatballs, 7));
+        }
+
+        [TestMethod]
+        public void FromFullFridgeRemoveExistingInventoryItem()
+        {
+            string invItem1 = "Meatballs";
+            string invItem2 = "Potato";
+            string invItem3 = "Pasta";
+            string invItem4 = "Sauce";
+            FridgeWorker currentFridge = new FridgeWorker();
+
+            currentFridge.AddIngredientToFridge(invItem1, 10);
+            currentFridge.AddIngredientToFridge(invItem2, 50);
+            currentFridge.AddIngredientToFridge(invItem3, 4);
+            currentFridge.AddIngredientToFridge(invItem4, 10);
+
+            Double result = currentFridge.TakeItemFromFridge(invItem1, 7);
+            Assert.AreEqual(3, result);
+
+            result = currentFridge.TakeItemFromFridge(invItem1, 7);
+            Assert.AreEqual(-4, result);
+        }
+
+        [TestMethod]
+        public void FromFullFridgeRemoveNonExistingInventoryItem()
+        {
+            string invItem1 = "Meatballs";
+            string invItem2 = "Potato";
+            string invItem3 = "Pasta";
+            string invRemoveItem = "Sauce";
+            FridgeWorker currentFridge = new FridgeWorker();
+
+            currentFridge.AddIngredientToFridge(invItem1, 10);
+            currentFridge.AddIngredientToFridge(invItem2, 50);
+            currentFridge.AddIngredientToFridge(invItem3, 4);
+
+            Double result = currentFridge.TakeItemFromFridge(invRemoveItem, 5);
+
+            Assert.AreEqual(-5, result);
+        }
+
+        [TestMethod]
+        public void FromEmptyFridgeRemoveInventoryItem()
+        {
+            FridgeWorker currentFridge = new FridgeWorker();
+            Double result = currentFridge.TakeItemFromFridge(inventoryItemMeatballs, 5);
+            Assert.AreEqual(-5, result);
+        }
+ 
+    }
+}
---
+++ FridgeWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Fridge;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{name}")]
+        public FridgeInventory Get(string name)
+        {
+
+            FridgeWorker myFridge = new FridgeWorker();
+
+            myFridge.AddIngredientToFridge("Falukorv", 10);
+            var result = myFridge.GetInventoryItem(name);
+
+            return result;
+        }
+
+        // GET api/values/5
+        [HttpGet("{name}/{quantity}")]
+        public bool Get(string name, double quantity)
+        {
+
+            FridgeWorker myFridge = new FridgeWorker();
+
+            myFridge.AddIngredientToFridge("Falukorv", 10);
+            var result = myFridge.IsItemAvailable(name, quantity);
+
+            return result;
+        }
+
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            // For more information on protecting this API from Cross Site Request Forgery (CSRF) attacks, see https://go.microsoft.com/fwlink/?LinkID=717803
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+            // For more information on protecting this API from Cross Site Request Forgery (CSRF) attacks, see https://go.microsoft.com/fwlink/?LinkID=717803
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            // For more information on protecting this API from Cross Site Request Forgery (CSRF) attacks, see https://go.microsoft.com/fwlink/?LinkID=717803
+        }
+    }
+}
